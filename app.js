d7a951f4402231023f13e1b5cfe98e9317566599
Thorsten <thorsten@potpiejimmy.de>
2016-05-06 23:41:42 +0200

    Initial import

---
+++ app.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+var principalAPI = require('./routes/principalAPI');
+
+var app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(config.db.url, function (err) {
+    if(err) {
+        console.log('Connection error', err);
+    } else {
+        console.log('Successfully connected to ' + config.db.url);
+        console.log('Opening route /');
+        app.use('/', principalAPI);
+    }
+});
+
+app.listen(3000, function () {
+  console.log('Startup complete. Listening on port 3000.');
+});
---
+++ routes/principalAPI.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var express = require('express');
+var schema = require('../models/schema.js');
+
+var Principal = schema.Principal;
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    Principal.find(function (err, principals) {
+        if (err) return console.error(err);
+        res.send(principals);
+    });
+});
+
+router.post('/', function (req, res) {
+    console.log(req.body);
+    var p = new Principal(req.body);
+    p.save(function (err, product, numAffected) {
+        if (err) res.send(err);
+        else res.send("Saved ID " + product._id + "\n");
+    });
+});
+
+module.exports = router;
